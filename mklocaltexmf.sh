c4af01e1639f1b6028a11eaecafd87519aae1daa
Alan Munn <amunn@msu.edu>
2018-09-06 23:01:30 -0400

    Updated .sh to be usable for Linux users too.

--- mklocaltexmf.sh
+++ mklocaltexmf.sh
@@ -1,36 +1,42 @@
 #!/bin/sh
-# Shell script to make a local texmf hierarchy for MacOS
+# Shell script to make a local texmf hierarchy for MacOS or Linux
 # Alan Munn <amunn@msu.edu>
 #
-# 2012/01/25
+# 2018/09/06
 #
-if [ ! -d ~/Library/texmf ]; then
+localdirname=`kpsewhich -var-value TEXMFHOME`
+if [ ! -d $localdirname ]; then
 
-	mkdir -p ~/Library/texmf/bibtex/bib
-	mkdir -p ~/Library/texmf/bibtex/bst
+	mkdir -p $localdirname/bibtex/bib
+	mkdir -p $localdirname/bibtex/bst
 
-	mkdir -p ~/Library/texmf/doc
+	mkdir -p $localdirname/doc
 
-	mkdir -p ~/Library/texmf/fonts/afm
-	mkdir -p ~/Library/texmf/fonts/map
-	mkdir -p ~/Library/texmf/fonts/misc
-	mkdir -p ~/Library/texmf/fonts/pk
-	mkdir -p ~/Library/texmf/fonts/source
-	mkdir -p ~/Library/texmf/fonts/tfm
-	mkdir -p ~/Library/texmf/fonts/type1
+	mkdir -p $localdirname/fonts/afm
+	mkdir -p $localdirname/fonts/map
+	mkdir -p $localdirname/fonts/misc
+	mkdir -p $localdirname/fonts/pk
+	mkdir -p $localdirname/fonts/source
+	mkdir -p $localdirname/fonts/tfm
+	mkdir -p $localdirname/fonts/type1
+	mkdir -p $localdirname/fonts/opentype
+	mkdir -p $localdirname/fonts/truetype
 
-	mkdir -p ~/Library/texmf/generic
+	mkdir -p $localdirname/generic
 
-	mkdir -p ~/Library/texmf/scripts
+	mkdir -p $localdirname/scripts
 
-	mkdir -p ~/Library/texmf/source
+	mkdir -p $localdirname/source
 
-	mkdir -p ~/Library/texmf/tex/context
-	mkdir -p ~/Library/texmf/tex/generic
-	mkdir -p ~/Library/texmf/tex/latex
-	mkdir -p ~/Library/texmf/tex/plain
-	mkdir -p ~/Library/texmf/tex/xelatex
-	mkdir -p ~/Library/texmf/tex/xetex
+	mkdir -p $localdirname/tex/context
+	mkdir -p $localdirname/tex/generic
+	mkdir -p $localdirname/tex/latex
+	mkdir -p $localdirname/tex/plain
+	mkdir -p $localdirname/tex/xelatex
+	mkdir -p $localdirname/tex/xetex
+	mkdir -p $localdirname/tex/luatex
+	mkdir -p $localdirname/tex/lualatex
+	
 	echo "Local texmf folder sucessfully created!"
 else
 echo "Local texmf folder already exists!"
